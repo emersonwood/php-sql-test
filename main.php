d03b5c562626db3caf66decc9a98faf60f1c3f2a
emersonwood <emersonwood94@gmail.com>
2017-09-01 12:03:31 +1200

    local

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+
+function ExecuteSQL ($query) {
+	return sprintf("Some result from \"%s\"\n\n", $query);
+}
+
+function Main() {
+	$getUserNamesSQL = "SELECT name FROM Users;";
+	$userNames = ExecuteSQL($getUserNamesSQL);
+	print $userNames;
+
+	print("Please enter your username: ");
+	$userName = rtrim(fgets(STDIN), "\n");
+	$getUserDataSQL = "SELECT * FROM Users WHERE UserId = " . $userName . ";";
+	$userData = ExecuteSQL($getUserDataSQL);
+	print $userData;
+}
+
+Main();
